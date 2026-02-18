cbebe8fca80ab3f939b24691885170ee27e89fdc
leylaomizrahi <leylaomizrahi@users.noreply.github.com>
2026-02-18 18:31:43 +0000

    fixed shape

--- app.js
+++ app.js
@@ -626,7 +626,7 @@ function buildSwipeCarousel(images, track){
     current = idx;
     slides[current].classList.add("isActive");
     const vw = window.innerWidth;
-    const slideW = vw * 0.64;
+    const slideW = vw * 0.55;
     const offset = (vw / 2) - (slideW * current) - (slideW / 2);
     track.style.transform = `translateX(${offset}px)`;
   }
